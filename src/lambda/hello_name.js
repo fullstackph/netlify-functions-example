48e1c90ef0849cd11b27d7632147f5f60c24b544
taze-fullstack <taze@fullstack.ph>
2018-09-28 16:07:15 +0800

    event as body

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -1,9 +1,8 @@
 exports.handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
-  console.log(event);
-  
+
   return {
     statusCode: 200,
-    body: `Hello, ${name}`
+    body: event
   };
 };

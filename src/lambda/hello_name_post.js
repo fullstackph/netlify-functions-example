95eacc6c5941657dd463b366fd8cb066c0a72046
taze-fullstack <taze@fullstack.ph>
2018-10-08 20:22:46 +0800

    console loh

--- src/lambda/hello_name_post.js
+++ src/lambda/hello_name_post.js
@@ -9,6 +9,7 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
+  console.log(params);
   const name = params.name || "World";
 
   return {
